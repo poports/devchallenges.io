e8245769583097674a0968444db876af45b71e6e
Robert Cortez <poports@gmail.com>
2020-09-27 14:55:06 +0800

    Changes after testing production build

--- AuthenticationApp/src/AuthServer/Frontend/src/index.js
+++ AuthenticationApp/src/AuthServer/Frontend/src/index.js
@@ -4,10 +4,13 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { GlobalStyle } from './components'
 
+const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href')
+const rootElement = document.getElementById('root')
+
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={baseUrl}>
     <App />
     <GlobalStyle />
   </BrowserRouter>,
-  document.getElementById('root')
+  rootElement
 )
